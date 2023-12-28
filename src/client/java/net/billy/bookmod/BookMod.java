34e5417534c3f2e80bcedc415df4b1164ef1d942
Billy Collins <ripmemesale12345@gmail.com>
2023-12-28 12:16:26 -0700

    work

---
+++ src/client/java/net/billy/bookmod/BookMod.java
@@ -0,0 +1,15 @@
+package net.billy.bookmod;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BookMod implements ModInitializer {
+	public static final String MOD_ID = "bookmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	@Override
+	public void onInitialize() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/net/billy/bookmod/BookModClient.java
@@ -0,0 +1,10 @@
+package net.billy.bookmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BookModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
